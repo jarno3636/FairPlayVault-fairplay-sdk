9f9a7a13c96cbd0f18e35aae1d3746d6d10b0624
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-29 10:52:05 -0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// src/index.ts
+export type { CreatePoolParams } from './types';
+export { FairplayClient } from './client';
+export { FAIRPLAY_VAULT_ABI } from './abi/FairplayVault';
+export { commitOf, randomSalt32 } from './utils';
